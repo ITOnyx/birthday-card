c08cd4ead2c389c2e5ee100165467c517cc64f58
ITOnyx <147623394+ITOnyx@users.noreply.github.com>
2025-10-05 21:53:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,112 +1,47 @@
-const contentLetterSrart_actived = "Gửi em yêu của anh." //Lời mở đầu cho bức thư
-const mainContentLetter = "Gửi lời nhắn nhủ đến người bạn bạn yêu thương." //Nội dung của bức thư
+const contentLetter = document.querySelector(".contentLetter");
+const sticker = document.querySelector(".sticker");
+const buttonGroup = document.querySelector(".button-group");
+const agreeBtn = document.getElementById("agreeBtn");
+const disagreeBtn = document.getElementById("disagreeBtn");
 
-// Gắn 1 đường link ảnh bất kì
-let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
-imgStart.src = "./img/cute-young-boy-kid-wearing-vest-and-hat-free-png.png";
-document.querySelector(".contentLetter").innerText = ""; // Để trống ban đầu
-document.querySelector(".mainContent").innerText = mainContentLetter;
-// Gắn 1 link ảnh bất kì
-let imgLetter = document.querySelector(".img");
-imgLetter.src = "./img/b4bbdb54b7152338d7143cb444a77f09.png"; //Hình ảnh xuất hiện trong nội dung của bức thư sau khi bức thư được viết ra hết
+const letterText = "Gửi người anh thương. Cảm ơn em đã đến với anh. Mỗi ngày bên em là một món quà quý giá. Anh yêu em… em có thể cho anh cơ hội được gọi em là người yêu nhé!!";
 
-const splitContentLetterSrart_actived = contentLetterSrart_actived.split("");
-document.querySelector(".letter-start").innerText = contentLetterSrart_actived;
-document.querySelector(".letter-content").innerText = mainContentLetter;
-document.querySelector(".sticker").addEventListener("click", function () { //Hiệu ứng gõ chữ cho phần mở đầu của bức thư
-    document.querySelector(".contentLetter").innerHTML = "";
-    document.querySelector(".startLetter").classList.add("active")
-    setTimeout(() => {
-        splitContentLetterSrart_actived.forEach((val, index) => {
-            setTimeout(() => {
-                document.querySelector(".contentLetter").innerHTML += val;
-                if (index == contentLetterSrart_actived.length - 1) {
-                    setTimeout(() => {
-                        document.querySelector(".recieve").setAttribute("style", "opacity: 1; transition: .5s") 
-                    }, 1000)
-                }
-            }, 50 * index)
-        })
-    }, 1000)
-})
+let letterShown = false; // 👉 cờ kiểm soát
 
-document.querySelector("#mess").addEventListener("change", function () { //Hiệu ứng gõ chữ cho phần nội dung của bức thư
-    if (this.checked == true) {
-        document.querySelector(".content").classList.add("actived")
-        const splitMainContentLetter = mainContentLetter.split("");
+// 👉 Click sticker -> bắt đầu gõ thư
+sticker.addEventListener("click", () => {
+  if (letterShown) return; // nếu đã hiện thì bỏ qua
 
-        splitMainContentLetter.forEach((val, index) => {
-            setTimeout(() => {
-                document.querySelector(".mainContent").innerHTML += val;
-                if (index == mainContentLetter.length - 1) {
-                    document.querySelector(".img1").setAttribute("style", "opacity: 1; transition: .5s")
-                }
-            }, 50 * index)
-        })
-
+  contentLetter.innerHTML = "";
+  let i = 0;
+  function typeChar() {
+    if (i < letterText.length) {
+      contentLetter.innerHTML += letterText[i];
+      i++;
+      setTimeout(typeChar, 50);
     } else {
-        document.querySelector(".content").classList.remove("actived")
-        document.querySelector(".img1").setAttribute("style", "opacity: 0; transition: .5s")
-        document.querySelector(".mainContent").innerHTML = "";
+      buttonGroup.classList.add("show"); // hiện nút sau khi gõ xong
+      letterShown = true; // đánh dấu đã hiện
     }
-})
+  }
+  typeChar();
+});
 
-document.querySelector(".recieve").addEventListener("click", () => {
-    document.querySelector(".startLetter").classList.add("close");
-    setTimeout(() => {
-        document.querySelector(".startForm").classList.add("close");
-        setTimeout(() => {
-            document.querySelector(".startForm").setAttribute("style", "bottom: 100%");
-            
-            let getTypeDevice = document.documentElement.clientWidth;
-            if (getTypeDevice <= 768) {
-                createLight(20)
-            } else {
-                createLight(40)
-            }
+// 👉 Đồng ý -> sang trang khác
+agreeBtn.addEventListener("click", () => {
+  window.location.href = "next.html"; 
+});
 
-        }, 500)
-    }, 500)
-})
+// 👉 Không đồng ý -> chạy trốn khi bấm
+disagreeBtn.addEventListener("click", () => {
+  const startLetter = document.querySelector('.startLetter');
+  const maxX = startLetter.offsetWidth - disagreeBtn.offsetWidth;
+  const maxY = startLetter.offsetHeight - disagreeBtn.offsetHeight - 50;
 
-// Animation Drop light _ Tạo hiệu ứng kim tuyến rơi
-//Bạn có thể thiết kế lại để trông chân thật hơn nhé, thiết kế của mình hơi bị cứng và thiếu sự tự nhiên
-const getBackground = document.querySelector(".backgroundParty");
-var width = getBackground.offsetWidth;
-var height = getBackground.offsetHeight;
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-function createLight(a) {
-    var container = document.querySelector(".backgroundParty");
-    const blurLv = [2, 4];
-    const count = a;
-    const allDefaultColor = ["red", "lime", "yellow", "orange", "blue"]
-
-    for (var i = 0; i < count; i++) {
-        var randomLeft = 0;
-        randomLeft = Math.floor(Math.random() * width);
-        var randomTop = 0;
-        randomTop = Math.floor(Math.random() * height / 2);
-        var color = "white";
-        var blur = Math.floor(Math.random() * 2);
-        var widthEle = Math.floor(Math.random() * 5) + 15;
-        var moveTime = Math.floor(Math.random() * 4) + 4;
-
-        var div = document.createElement("div");
-        div.classList.add("snow");
-        div.style.position = "absolute";
-        div.style.backgroundColor = allDefaultColor[Math.floor(Math.random() * 5)]
-        div.style.borderRadius = Math.floor(Math.random() * 10 + 10).toString() + "px"
-
-        div.style.height = "0px";
-        div.style.width = "0px";
-
-        div.style.height = widthEle * Math.floor(Math.random() * 4 + 1) + "px";
-        div.style.width = widthEle + "px";
-        div.style.marginLeft = randomLeft + "px"
-        div.style.marginTop = randomTop + "px"
-        div.style.filter = "blur(" + blurLv[blur] + "px" + ")"
-        div.style.animation = "moveLight " + moveTime + "s ease-in-out infinite";
-
-        container.appendChild(div);
-    }
-}
+  disagreeBtn.style.position = "absolute";
+  disagreeBtn.style.left = randomX + 'px';
+  disagreeBtn.style.top = randomY + 'px';
+});
